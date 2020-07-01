d10efee17344644c6e4ce2de8a5af04c28cabf1e
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2020-07-01 15:55:10 +0800

    数据清洗，获取每年6.28最高最低气温

---
+++ data-cleaning.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from datetime import datetime
+from dateutil import parser
+
+data_raw = pd.read_csv('2200728.csv', encoding='utf-8')
+data_raw['date'] = data_raw['DATE'].apply(parser.parse)
+data_raw['tmax'] = data_raw['TMAX'].astype(float)
+data_raw['tmin'] = data_raw['TMIN'].astype(float)
+data = data_raw.loc[:, ['date', 'tmax', 'tmin']]
+data = data[(pd.Series.notnull(data['tmax'])) & (pd.Series.notnull(data['tmin']))]	#把空数据过滤掉
+
+data = data[(data['date'] >= datetime(1980,1, 1)) & (data['date'] <= datetime(2016,1, 1))]
+data.query("date.dt.day == 28 & date.dt.month == 6", inplace=True) #得到历史上每年 6.28 的数据
+data.to_csv('maxmin.csv',index=None)
