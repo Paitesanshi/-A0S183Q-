bfd7d12a77b6ef5a66c53fa8442e22403a63d599
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2020-07-03 11:02:49 +0800

    ARIMA模型网格搜索参数，MSE计算实现

--- getARIMA.py
+++ getARIMA.py
@@ -1,33 +1,34 @@
 import warnings
-import pandas as pd
-from datetime import datetime
+import pandas as pd;
 from dateutil import parser
+from pandas import Series
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 
-
-# evaluate an ARIMA model for a given order (p,d,q)
+# 评估给定订单的ARIMA模型（p，d，q）
 def evaluate_arima_model(X, arima_order):
-    # 读取测试集数据
+    # prepare training dataset
+
     train_size = int(len(X) * 0.66)
+
     train, test = X[0:train_size], X[train_size:]
     history = [x for x in train]
-    # 预测
+    # 进行预测
     predictions = list()
     for t in range(len(test)):
         model = ARIMA(history, order=arima_order)
         model_fit = model.fit(disp=0)
-        yhat = model_fit.forecast()[0]
+        yhat = model_fit.forecast(steps=1)[0]
         predictions.append(yhat)
-        history.append(test[t])
-    # calculate out of sample error
+       # history.append(test[t+train_size])
+    # 计算样本的误差值
     error = mean_squared_error(test, predictions)
     return error
 
 
-# evaluate combinations of p, d and q values for an ARIMA model
+# 评估ARIMA模型的p，d和q值的组合
 def evaluate_models(dataset, p_values, d_values, q_values):
-    #dataset = dataset.astype('float32')
+    dataset = dataset.astype('float32')
     best_score, best_cfg = float("inf"), None
     for p in p_values:
         for d in d_values:
@@ -40,20 +41,13 @@ def evaluate_models(dataset, p_values, d_values, q_values):
                     print('ARIMA%s MSE=%.3f' % (order, mse))
                 except:
                     continue
-    print('Best ARIMA %s MSE=%.3f' % (best_cfg, best_score))
 
-
+    print('Best ARIMA%s MSE=%.3f' % (best_cfg, best_score))
 # load dataset
-def parser(x):
-    return datetime.strptime( x, '%Y-%m')
-
-
-# series = read_csv('maxmin.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-# evaluate parameters
-
-data = pd.read_csv('maxmin.csv', header=0)
-p_values = range(1, 5)
+series = pd.read_csv('maxmin.csv', header=0)
+# 评估参数
+p_values = range(0,5)
 d_values = range(0, 3)
-q_values = range(1, 5)
+q_values = range(0, 5)
 warnings.filterwarnings("ignore")
-evaluate_models(data, p_values, d_values, q_values)
+evaluate_models(series['tmax'], p_values, d_values, q_values)
