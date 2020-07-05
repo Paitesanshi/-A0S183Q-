a36b893e31842ef5214ece8893dd1028205943c4
odin <shency99@163.com>
2020-07-06 01:54:03 +0800

    .

--- wash.py
+++ wash.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import time
+import csv
 from collections import namedtuple
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,29 +8,25 @@ import matplotlib.pyplot as plt
 features = ["DATE", "PRCP", "PRCP_ATTRIBUTES", "SNWD", "SNWD_ATTRIBUTES", "TAVG", "TAVG_ATTRIBUTES",
             "TMAX", "TMAX_ATTRIBUTES", "TMIN", "TMIN_ATTRIBUTES"]
 DailySummary = namedtuple("DailySummary", features)
-def read_weather_data(url, api_key, target_date, days):
+def read_weather_data(csvname):
     records = []
-    for _ in range(days):
-        request = BASE_URL.format(API_KEY, target_date.strftime('%Y%m%d'))
-        response = requests.get(request)
-        if response.status_code == 200:
-            data = response.json()['history']['dailysummary'][0]
-            records.append(DailySummary(
-                date=target_date,
-                meantempm=data['meantempm'],
-                meandewptm=data['meandewptm'],
-                meanpressurem=data['meanpressurem'],
-                maxhumidity=data['maxhumidity'],
-                minhumidity=data['minhumidity'],
-                maxtempm=data['maxtempm'],
-                mintempm=data['mintempm'],
-                maxdewptm=data['maxdewptm'],
-                mindewptm=data['mindewptm'],
-                maxpressurem=data['maxpressurem'],
-                minpressurem=data['minpressurem'],
-                precipm=data['precipm']))
-        time.sleep(6)
-        target_date += timedelta(days=1)
-    return records
+    csvFile = open(csvname, "r")
+    reader = csv.reader(csvFile)
+    for item in reader:
+        # 忽略第一行
+        if reader.line_num == 1:
+            continue
+        print(item[0])
+        records[item[0]] = item[1]
 
-df = pd.DataFrame(records, columns=features).set_index('date')
+    csvFile.close()
+    return records
+def derive_nth_day_feature(df, feature, N):
+    rows = df.shape[0]
+    nth_prior_measurements = [None]*N + [df[feature][i-N] for i in range(N, rows)]
+    col_name = "{}_{}".format(feature, N)
+    df[col_name] = nth_prior_measurements
+
+
+df = pd.DataFrame(read_weather_data('2199262.csv'), columns=features).set_index('DATE')
+tmp = df[['TMAX', 'TMIN']].head(10)
