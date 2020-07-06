6cfa804784f9a128cb19a4939ca3e7cbce65a836
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2020-07-06 10:24:36 +0800

    prophet高低温度添加

--- prophet.py
+++ prophet.py
@@ -8,23 +8,26 @@ data_raw['tmax'] = data_raw['TMAX'].astype(float)
 data_raw['tmin'] = data_raw['TMIN'].astype(float)
 data = data_raw.loc[:, ['date', 'tmax', 'tmin']]
 data = data[(pd.Series.notnull(data['tmax'])) & (pd.Series.notnull(data['tmin']))]  # 把空数据过滤掉
-df = pd.DataFrame()
-df['ds'] = data['date']
-df['y'] = data['tmin']
+dfmin = pd.DataFrame()
+dfmin['ds'] = data['date']
+dfmin['y'] = data['tmin']
+dfmax = pd.DataFrame()
+dfmax['ds'] = data['date']
+dfmax['y'] = data['tmax']
 
 # 3、训练集、测试集划分
-trainsize = int(df.shape[0]*0.9)
-train = df[0:trainsize]
-test = df[trainsize:df.shape[0]]
+trainsize = int(dfmin.shape[0]*0.9)
+train = dfmin[0:trainsize]
+test = dfmin[trainsize:dfmin.shape[0]]
 
 # 4、模型训练
 # changepoint_prior_scale默认为0.05,增大changepoint_prior_scale表示让模型拟合更多，可能发生过拟合风险
 model = Prophet(changepoint_prior_scale=0.5)
-model.fit(df);
+model.fit(dfmin);
 
 #5、模型预测
 # 构建待预测日期数据框，periods = 365 代表除历史数据的日期外再往后推 365 天
-future = model.make_future_dataframe(periods=df.shape[0]-trainsize)
+future = model.make_future_dataframe(periods=dfmin.shape[0]-trainsize)
 print("-----------------future.tail-----------------")
 print(future.tail())
 
@@ -38,6 +41,45 @@ model.plot(forecast);
 
 # 预测的成分分析绘图，展示预测中的趋势、周效应和年度效应
 model.plot_components(forecast);
+graph = plt.figure(figsize=(10, 4))
+ax = graph.add_subplot(1,1,1)
+#ax.set(title='Total_Purchase_Amt', ylabel='Unit (Fahrenheit)', xlabel='Date')
+plt.plot(forecast['ds'], forecast['yhat'])
 plt.show()
 #6、模型评估
-print("mse is",mean_squared_error(test['y'].values,forecast['yhat'].values[trainsize:df.shape[0]]))
+print("mse is",mean_squared_error(test['y'].values,forecast['yhat'].values[trainsize:dfmin.shape[0]]))
+
+
+
+trainsize = int(dfmax.shape[0]*0.9)
+train = dfmax[0:trainsize]
+test = dfmax[trainsize:dfmax.shape[0]]
+
+# 4、模型训练
+# changepoint_prior_scale默认为0.05,增大changepoint_prior_scale表示让模型拟合更多，可能发生过拟合风险
+model = Prophet(changepoint_prior_scale=0.5)
+model.fit(dfmax);
+
+#5、模型预测
+# 构建待预测日期数据框，periods = 365 代表除历史数据的日期外再往后推 365 天
+future = model.make_future_dataframe(periods=dfmax.shape[0]-trainsize)
+print("-----------------future.tail-----------------")
+print(future.tail())
+
+# 预测数据集
+forecast = model.predict(future)
+print("-----------------forcast tail-----------------")
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+
+# 展示预测结果
+model.plot(forecast);
+
+# 预测的成分分析绘图，展示预测中的趋势、周效应和年度效应
+model.plot_components(forecast);
+graph = plt.figure(figsize=(10, 4))
+ax = graph.add_subplot(1,1,1)
+#ax.set(title='Total_Purchase_Amt', ylabel='Unit (Fahrenheit)', xlabel='Date')
+plt.plot(forecast['ds'], forecast['yhat'])
+plt.show()
+#6、模型评估
+print("mse is",mean_squared_error(test['y'].values,forecast['yhat'].values[trainsize:dfmax.shape[0]]))
