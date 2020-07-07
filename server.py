cf38163f794dda571f8e4c96e245f6f2bc969be9
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2020-07-07 17:47:55 +0800

    网络添加

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
-# encoding:utf-8
-# !/usr/bin/env python
-import psutil
-import time
+#!/usr/bin/env python
 from threading import Lock
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-import json
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room,close_room, rooms, disconnect
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
 async_mode = None
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
@@ -14,20 +15,15 @@ thread = None
 thread_lock = Lock()
 
 
-# 后台线程 产生数据，即刻推送至前端
 def background_thread():
+    """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        socketio.sleep(5)
+        socketio.sleep(10)
         count += 1
-        t = time.strftime('%M:%S', time.localtime())
-        # 获取系统时间（只取分:秒）
-        cpus = psutil.cpu_percent(interval=None, percpu=True)
-        # 获取系统cpu使用率 non-blocking
-        socketio.emit('server_response',
-                      {'data': [t, cpus], 'count': count},
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count},
                       namespace='/test')
-        # 注意：这里不需要客户端连接的上下文，默认 broadcast = True
 
 
 @app.route('/')
@@ -35,18 +31,68 @@ def index():
     return render_template('testclient.html', async_mode=socketio.async_mode)
 
 
-@socketio.on('my_event', namespace='/test')
-def getarea(data):
-    area=json.loads(data)
-    print(area)
+class MyNamespace(Namespace):
+    def on_my_event(self, message):
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': message['data'], 'count': session['receive_count']})
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(target=background_thread)
+    def on_my_broadcast_event(self, message):
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': message['data'], 'count': session['receive_count']},
+             broadcast=True)
+
+    def on_join(self, message):
+        join_room(message['room'])
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': 'In rooms: ' + ', '.join(rooms()),
+              'count': session['receive_count']})
+
+    def on_leave(self, message):
+        leave_room(message['room'])
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': 'In rooms: ' + ', '.join(rooms()),
+              'count': session['receive_count']})
+
+    def on_close_room(self, message):
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
+                             'count': session['receive_count']},
+             room=message['room'])
+        close_room(message['room'])
+
+    def on_my_room_event(self, message):
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': message['data'], 'count': session['receive_count']},
+             room=message['room'])
+
+    def on_disconnect_request(self):
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': 'Disconnected!', 'count': session['receive_count']})
+        disconnect()
+
+    def on_my_ping(self):
+        emit('my_pong')
+
+    def on_connect(self):
+        global thread
+        with thread_lock:
+            if thread is None:
+                thread = socketio.start_background_task(background_thread)
+        emit('my_response', {'data': 'Connected', 'count': 0})
+
+    def on_disconnect(self):
+        print('Client disconnected', request.sid)
+
+
+socketio.on_namespace(MyNamespace('/test'))
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
+   # socketio.run(app, debug=True)
--- webindex.py
+++ webindex.py
@@ -1,83 +1,125 @@
 from flask import Flask
+import pandas as pd
 from flask import render_template, redirect, url_for, request, session
 import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, emit
-import flask
 from flask_socketio import SocketIO
 import json
 import psutil
+import pinyin.cedict
+
 app = Flask(__name__)
 app.config.from_object(config)
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
+name = str
+data_raw = []
+
 
 @app.route('/china')
 def china():
     return render_template('china.html')
 
+
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('china'))
+
 
 @app.route('/infor')
 def infor():
     return render_template('check.html')
 
-@socketio.on('my_event', namespace='/test')
-def getarea(data):
-    area=json.loads(data)
-    print(area)
+
+@socketio.on('name', namespace='/test')
+def test_getarea(data):
+    area = json.loads(data)
+    print(area['area'])
+    emit('server_response', "456")
+    data_raw = pd.read_csv(area['area'] + "_min.csv")
+    print(data_raw)
+
+
+@socketio.on('date', namespace='/test')
+def test_getdate(data):
+    area = json.loads(data)
+    print(area['date'])
+    temp = {22, 33, 55, 44, 66, 77, 99}
+    data_raw = pd.read_csv(area['area'] + "_min.csv")
+    pos = data_raw[data_raw.date == area['date']].index
+    temp = data_raw[pos:pos + 7]
+    print(temp)
+    emit('server_response', json.dumps(temp))
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    print("connected")
+    emit('server_response', "123")
+
+
+#
+# @socketio.on('json', namespace='/test')
+# def handle_json(json):
+#     print('received json: ' + str(json))
+# @socketio.on('message',namespace="/test")
+# def handle_message(message):
+#     print('received message:　' + message)
+
 # 登陆
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('base.html')
     else:
         username = request.form.get('id')  # 与html页面名字相同
         password = request.form.get('password')
-        print(username,password)
-        user = User.query.filter(User.username == username,User.password==password).first()
+        print(username, password)
+        user = User.query.filter(User.username == username, User.password == password).first()
         print(user)
         if user:
-            session['username']=username
-            session.permanent=True
+            session['username'] = username
+            session.permanent = True
             return redirect(url_for('china'))
         else:
             return '用户不存在'
+
+
 # 注册
-@app.route('/regis',methods=['GET','POST'])
+@app.route('/regis', methods=['GET', 'POST'])
 def register():
-    if request.method=='GET':
+    if request.method == 'GET':
 
         return render_template('zhuce1.html')
     else:
 
-        username=request.form.get('zcid')#与html页面名字相同
-        password=request.form.get('zcpassword')
-        user=User.query.filter(User.username==username).first()
+        username = request.form.get('zcid')  # 与html页面名字相同
+        password = request.form.get('zcpassword')
+        user = User.query.filter(User.username == username).first()
         if user:
             return 'exit'
         else:
-            user=User(username=username,password=password)
+            user = User(username=username, password=password)
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('login'))
 
 
-
 # 开始创建用户模型，即在数据库中创建表格：
 class User(db.Model):
-    __tablename__='user'
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    username=db.Column(db.String(20),nullable=False)
-    password=db.Column(db.String(20),nullable=False)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
 
-#创建表格
+
+# 创建表格
 db.create_all()
 
-#数据查询方法
+# 数据查询方法
 User.query.filter(User.username == 'mis1114').first()
+
 
 # #数据添加方法
 # user = User(username='wl097tql',password='g6666')
@@ -96,13 +138,14 @@ User.query.filter(User.username == 'mis1114').first()
 
 @app.context_processor
 def mycontext():
-    username=session.get('user')
+    username = session.get('user')
     if username:
-        return {'username':username}
+        return {'username': username}
     else:
         return {}
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # web服务器入口
-    app.run(debug=True)
+    #app.run(debug=True)
+    socketio.run(app, debug=True)
