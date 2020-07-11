d86a9ca178bae3f2dea41e07efe3455c261179b1
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2020-07-11 19:16:12 +0800

    config密码修改

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zxc110@localhost:3306/test_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:wyj18301109@localhost:3306/test_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 import os
@@ -9,7 +9,7 @@ SECRET_KEY = 'os.urandom(24)'
 DIALECT = 'mysql'
 DRIVER = 'mysqldb'
 USERNAME = 'root'
-PASSWORD = 'zxc110'
+PASSWORD = 'wyj18301109'
 HOST = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'test_db'
